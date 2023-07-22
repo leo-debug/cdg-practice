c2a8524e7515fb0dc7e4f6011b0efebd96f41029
Andrei <sam.andriks@gmail.com>
2023-07-22 16:55:56 +0300

    greeting_2.0 method completed

--- 2-lesson/greeting.rb
+++ 2-lesson/greeting.rb
@@ -1,25 +1,29 @@
-# очистка строки ввода ФИО от недопустимых символов
+# очистка строки ввода ФИО от недопустимых символов, цифр
 # и перевод первого символа в верхний регистр
-def fio_line_cleaning string
-    string = gets.gsub(/[^A-Za-z]/, '').capitalize
+def fio_line_cleaning (string)
+    string = string.tr('^A-Za-z', '').capitalize
 
     #return string
 end
 
+# очистка строки ввода ФИО от недопустимых символов/букв, кроме цифр
+def age_line_cleaning (age)
+    age = age.tr('^0-9', '').to_i
+
+    #return age
+end
+
+#преобразуем введенные данные в строку и передаем в метод очистки
 puts "Введите ваше имя:"
-#first_name = fio_line_cleaning(gets.to_s)
-
-first_name = gets.to_s.chop
-#first_name = fio_line_cleaning(first_name)
-
-
+puts first_name = fio_line_cleaning(gets.to_s)
 
 puts "Введите вашу фамилию:"
-last_name = gets.to_s.chop
-#last_name = fio_line_cleaning(gets)
+last_name = fio_line_cleaning(gets.to_s)
 
+#преобразуем введенные данные в строку и передаем в метод очистки
 puts "Склько тебе лет?"
-age = gets.to_i
+age = age_line_cleaning(gets.to_s)
+
 
 if age>=18
     puts "Привет, #{first_name} #{last_name}. Самое время заняться делом!"
